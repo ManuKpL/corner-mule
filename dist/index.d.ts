89861cbf3f456ee688b5ceda6d7bd8c63ed31e80
Emmanuel Capelle <emmanuel.capelle@gmail.com>
2020-02-15 11:25:05 +0100

    conf(general) init ts project :wrench:

---
+++ dist/index.d.ts
@@ -0,0 +1,1 @@
+export declare const TEST_MULE = "corner-mule";
---
+++ dist/index.js
@@ -0,0 +1,4 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.TEST_MULE = "corner-mule";
+//# sourceMappingURL=index.js.map
