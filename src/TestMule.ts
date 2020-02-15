257e27e99f77394c6f6afa24033274324c22d650
Emmanuel Capelle <emmanuel.capelle@gmail.com>
2020-02-15 20:21:44 +0100

    feat(TestMule) implemented addSpy() method

--- src/TestMule.ts
+++ src/TestMule.ts
@@ -1,6 +1,28 @@
 export class TestMule<T = any> {
   public instance!: T;
-
-  public readonly spies: { [key: string]: jest.SpyInstance } = {};
   public readonly values: { [key: string]: any } = {};
+
+  private readonly spiesMap = new Map<string, jest.SpyInstance>();
+
+  public get spies(): { [key: string]: jest.SpyInstance } {
+    return Object.fromEntries(this.spiesMap);
+  }
+
+  public addSpy(
+    source: { [key: string]: any },
+    methodName: string,
+    spyName = methodName,
+  ): TestMule {
+    this.ensureUniqSpies(spyName);
+    this.spiesMap.set(spyName, jest.spyOn(source, methodName));
+    return this;
+  }
+
+  private ensureUniqSpies(spyName: string): void {
+    if (this.spiesMap.has(spyName)) {
+      throw new Error(
+        `A spy with the name [${spyName}] already exists in this test mule`,
+      );
+    }
+  }
 }
--- src/__tests__/TestMule.spec.ts
+++ src/__tests__/TestMule.spec.ts
@@ -1,31 +1,80 @@
 import { TestMule } from '../TestMule';
 
 describe('TestMule', () => {
-  let testSuite: { instance?: TestMule } = {};
+  let testSuite: {
+    instance?: TestMule;
+    spySource?: { foo: () => number };
+  } = {};
 
-  beforeAll(() => {
+  beforeEach(() => {
     testSuite.instance = new TestMule();
+    testSuite.spySource = {
+      foo() {
+        return 0;
+      },
+    };
   });
 
   afterAll(() => {
     testSuite = null;
   });
 
-  test('should be defined', () => {
-    expect(TestMule).toBeDefined();
+  describe('initialization', () => {
+    test('should be defined', () => {
+      expect(TestMule).toBeDefined();
+    });
+
+    test('should create', () => {
+      expect(testSuite.instance).toBeTruthy();
+    });
+
+    test('should be created with an empty wrapping object for spies', () => {
+      expect(testSuite.instance.spies).toBeTruthy();
+      expect(testSuite.instance.spies).toBeInstanceOf(Object);
+    });
+
+    test('should be created with an empty wrapping object for values', () => {
+      expect(testSuite.instance.values).toBeTruthy();
+      expect(testSuite.instance.values).toBeInstanceOf(Object);
+    });
   });
 
-  test('should create', () => {
-    expect(testSuite.instance).toBeTruthy();
-  });
+  describe('addSpy()', () => {
+    test('should return the instance of TestMule to allow chaining', () => {
+      expect(
+        testSuite.instance.addSpy(testSuite.spySource, 'foo'),
+      ).toStrictEqual(testSuite.instance);
+    });
 
-  test('should be created with an empty wrapping object for spies', () => {
-    expect(testSuite.instance.spies).toBeTruthy();
-    expect(testSuite.instance.spies).toBeInstanceOf(Object);
-  });
+    test('given an objet and a method name to spy on, adds a spy to the spies map using the method name', () => {
+      testSuite.instance.addSpy(testSuite.spySource, 'foo');
+      expect(Object.entries(testSuite.instance.spies)).toHaveLength(1);
+      expect(testSuite.instance.spies.foo).toBeDefined();
+    });
 
-  test('should be created with an empty wrapping object for values', () => {
-    expect(testSuite.instance.values).toBeTruthy();
-    expect(testSuite.instance.values).toBeInstanceOf(Object);
+    test('given an objet, a method name and a spy name, adds a spy to the spies map using the spy name', () => {
+      testSuite.instance.addSpy(testSuite.spySource, 'foo', 'bar');
+      expect(Object.entries(testSuite.instance.spies)).toHaveLength(1);
+      expect(testSuite.instance.spies.bar).toBeDefined();
+      expect(testSuite.instance.spies.foo).not.toBeDefined();
+    });
+
+    test('given a method name matching an existing spy, then throws an error', () => {
+      testSuite.instance.addSpy(testSuite.spySource, 'foo');
+      expect(() =>
+        testSuite.instance.addSpy(testSuite.spySource, 'foo'),
+      ).toThrowError(
+        'A spy with the name [foo] already exists in this test mule',
+      );
+    });
+
+    test('given a spy name matching an existing spy, then throws an error', () => {
+      testSuite.instance.addSpy(testSuite.spySource, 'foo', 'bar');
+      expect(() =>
+        testSuite.instance.addSpy(testSuite.spySource, 'foo', 'bar'),
+      ).toThrowError(
+        'A spy with the name [bar] already exists in this test mule',
+      );
+    });
   });
 });
